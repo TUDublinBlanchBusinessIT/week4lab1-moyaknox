a2138b8486f8638a2a7c4b2a3e3cd6bddbc67af9
B00153832 Moya Knox <B00153832@mytudublin.ie>
2025-02-17 15:29:21 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create');
